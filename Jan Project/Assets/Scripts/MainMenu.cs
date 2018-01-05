0a2fcf3f014a440c03ed0553a621edf17e757e97
18jwong <34906863+18jwong@users.noreply.github.com>
2018-01-04 17:19:20 -0800

    Started the pick name screen
    Names can be selected, but are not stored in any way yet.

---
+++ Jan Project/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu: MonoBehaviour
+{
+    //Pressing the Play Button causes Unity to load the next scene
+    public void PlayGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    //Pressing the Quit Button causes Unity to close the application
+    public void QuitGame()
+    {
+        //Debug prints to console so you can see in the Unity editor
+        Debug.Log("Quit");
+        Application.Quit();
+    }
+}
