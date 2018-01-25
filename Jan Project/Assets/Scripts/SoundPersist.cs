5392bd07bc8f65ba4797aac4699ac0714592d2f4
18jwong <18jwong@shcp.edu>
2018-01-24 22:38:55 -0800

    Redid Music ingame
    Changed Music to MS playbacks
    Added them to volume slider
    Music persists through scenes

---
+++ Jan Project/Assets/Scripts/SoundPersist.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+using System;
+
+public class SoundPersist : MonoBehaviour {
+
+    public static SoundPersist instance;
+
+    // Use this for initialization
+    void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else {
+            Destroy(gameObject);
+            return;
+        }
+
+        DontDestroyOnLoad(gameObject);
+    }
+}
